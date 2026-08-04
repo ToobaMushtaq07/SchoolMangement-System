4a0002898ed333442dbda5228718e96c6f30ef04
ToobaMushtaq07 <mtooba237@gmail.com>
2026-08-04 05:55:42 +0500

    Added Person and Student modules using OOP

---
+++ package/person.py
@@ -0,0 +1,10 @@
+class Person:
+
+    def _init_(self, person_id, name):
+        self.person_id = person_id
+        self.name = name
+
+    def display_person(self):
+        print("\nPerson Details")
+        print(f"ID   : {self.person_id}")
+        print(f"Name : {self.name}")
---
+++ package/student.py
@@ -0,0 +1,123 @@
+from package.person import Person
+import os
+
+class Student(Person):
+
+    FILE_NAME = "data/students.txt"
+
+    def _init_(self, person_id, name, age, department):
+        super()._init_(person_id, name)
+        self.age = age
+        self.department = department
+
+    # Add Student
+    def add_student(self):
+        try:
+            with open(Student.FILE_NAME, "a") as file:
+                file.write(f"{self.person_id},{self.name},{self.age},{self.department}\n")
+            print("Student added successfully!")
+
+        except Exception as e:
+            print("Error:", e)
+
+    # View Students
+    def view_students():
+        try:
+            if not os.path.exists(Student.FILE_NAME):
+                print("No student record found.")
+                return
+
+            with open(Student.FILE_NAME, "r") as file:
+                records = file.readlines()
+
+            if len(records) == 0:
+                print("No student record found.")
+                return
+
+            print("\n========== Student Records ==========")
+
+            for line in records:
+                person_id, name, age, department = line.strip().split(",")
+
+                print(f"Student ID : {person_id}")
+                print(f"Name       : {name}")
+                print(f"Age        : {age}")
+                print(f"Department : {department}")
+                print("-------------------------------------")
+
+        except FileNotFoundError:
+            print("Student file not found.")
+
+        except Exception as e:
+            print("Error:", e)
+
+    # Search Student
+    def search_student():
+
+        try:
+            search_id = input("Enter Student ID: ")
+
+            if not os.path.exists(Student.FILE_NAME):
+                print("Student file not found.")
+                return
+
+            found = False
+
+            with open(Student.FILE_NAME, "r") as file:
+
+                for line in file:
+
+                    person_id, name, age, department = line.strip().split(",")
+
+                    if person_id == search_id:
+
+                        print("\nStudent Found")
+                        print("----------------------")
+                        print("Student ID :", person_id)
+                        print("Name       :", name)
+                        print("Age        :", age)
+                        print("Department :", department)
+
+                        found = True
+                        break
+
+            if not found:
+                print("Student not found.")
+
+        except Exception as e:
+            print("Error:", e)
+
+    # Delete Student
+    def delete_student():
+
+        try:
+            delete_id = input("Enter Student ID to delete: ")
+
+            if not os.path.exists(Student.FILE_NAME):
+                print("Student file not found.")
+                return
+
+            with open(Student.FILE_NAME, "r") as file:
+                records = file.readlines()
+
+            found = False
+
+            with open(Student.FILE_NAME, "w") as file:
+
+                for line in records:
+
+                    person_id = line.strip().split(",")[0]
+
+                    if person_id != delete_id:
+                        file.write(line)
+                    else:
+                        found = True
+
+            if found:
+                print("Student deleted successfully!")
+
+            else:
+                print("Student not found.")
+
+        except Exception as e:
+            print("Error:", e)
